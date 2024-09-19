038b3ab6167007d2574a28de52eff0e56563b877
vaibhu9 <vaibhavshejol9@gmail.com>
2024-09-20 00:35:56 +0530

    project added

---
+++ src/main/java/com/amazingcode/in/tms/club/TmsClubAppApplication.java
@@ -0,0 +1,13 @@
+package com.amazingcode.in.tms.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsClubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsClubAppApplication.class, args);
+	}
+
+}
