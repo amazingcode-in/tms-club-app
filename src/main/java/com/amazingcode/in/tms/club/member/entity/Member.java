90e65fa381211d2410e9da0ae45393879a3a4742
amazingcode-in <amazingcode.in@gmail.com>
2024-09-26 23:10:37 +0530

    member entity created

---
+++ src/main/java/com/amazingcode/in/tms/club/member/entity/Member.java
@@ -0,0 +1,53 @@
+package com.amazingcode.in.tms.club.member.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @Column(name = "profession")
+    private String profession;
+
+    @Column(name = "club_joining_date")
+    private LocalDate clubJoiningDate;
+}
