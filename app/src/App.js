6da0f28c19ee7e3a82737e5a2983d73d56d27873
vaibhu9 <vaibhavshejol9@gmail.com>
2024-10-03 00:30:26 +0530

    url mentioned

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {  } from 'bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MemberProfile from './component/member/MemberProfile';
+import Login from './component/navigation/Login';
+import AdminPanel from './component/admin/AdminPanel';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* navigation section */}
+        <Route path='/' element = {<Login/>}/>
+
+        {/* admin section */}
+        <Route path='/admin-panel' element = {<AdminPanel/>}/>
+
+        {/* member section */}
+        <Route path='/member-profile' element = {<MemberProfile/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
