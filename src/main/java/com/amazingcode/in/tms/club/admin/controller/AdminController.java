76eb1df0113febf97ed19c0ec730498e07013c25
amazingcode-in <amazingcode.in@gmail.com>
2024-10-07 11:37:19 +0530

    crossorigin added

--- src/main/java/com/amazingcode/in/tms/club/admin/controller/AdminController.java
+++ src/main/java/com/amazingcode/in/tms/club/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.amazingcode.in.tms.club.admin.entity.Admin;
 import com.amazingcode.in.tms.club.admin.service.AdminService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/amazingcode.in.tms.club/admin")
 public class AdminController {
 
